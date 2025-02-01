cd5fe32885879e94e5d847b14470cfe9c289d1c0
Pablo-fp <pablofp1988@hotmail.com>
2025-02-01 23:10:27 +0100

    Exercises 6.1.-6.2.

---
+++ unicafe-redux/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+const App = () => {
+  const voteFor = (type) => {
+    store.dispatch({
+      type: type
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={() => voteFor('GOOD')}>good</button>
+      <button onClick={() => voteFor('OK')}>ok</button>
+      <button onClick={() => voteFor('BAD')}>bad</button>
+      <button onClick={() => voteFor('ZERO')}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
