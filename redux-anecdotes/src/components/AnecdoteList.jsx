0023ce0cd31f9411614ed08dcfa4c6a04ec3ce64
Pablo-fp <pablofp1988@hotmail.com>
2025-02-08 04:23:35 +0100

    6.19 Anecdotes and the Backend, step 6

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -4,24 +4,31 @@ import { voteAnecdote } from '../reducers/anecdoteReducer';
 import { setNotification } from '../reducers/notificationReducer';
 import Notification from './Notification';
 
+// Anecdote component represents a single anecdote item
 const Anecdote = (props) => {
   const dispatch = useDispatch();
+
+  // Function to handle voting for an anecdote
   const vote = async (anecdote) => {
-    dispatch(voteAnecdote(anecdote.id));
-    dispatch(setNotification(`You voted for '${anecdote.content}'`, 5));
+    dispatch(voteAnecdote(anecdote.id)); // Dispatch vote action
+    dispatch(setNotification(`You voted for '${anecdote.content}'`, 5)); // Dispatch notification action
   };
+
   return (
     <>
-      <div>{props.anecdote.content}</div>
+      <div>{props.anecdote.content}</div> {/* Display anecdote content */}
       <div>
-        has {props.anecdote.votes}
-        <button onClick={() => vote(props.anecdote)}>vote</button>
+        has {props.anecdote.votes} {/* Display number of votes */}
+        <button onClick={() => vote(props.anecdote)}>vote</button>{' '}
+        {/* Button to vote for the anecdote */}
       </div>
     </>
   );
 };
 
+// AnecdoteList component represents the list of anecdotes
 const AnecdoteList = () => {
+  // Select anecdotes from the Redux store and filter them based on the filter state
   const anecdotes = useSelector(({ anecdotes, filter }) => {
     return anecdotes.filter((anecdote) =>
       anecdote.content.toLowerCase().includes(filter.toLowerCase())
@@ -32,9 +39,7 @@ const AnecdoteList = () => {
     <div>
       <Notification />
       {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <Anecdote anecdote={anecdote} />
-        </div>
+        <Anecdote key={anecdote.id} anecdote={anecdote} />
       ))}
     </div>
   );
